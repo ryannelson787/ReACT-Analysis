2e3fa275a514736ec69c3ce407fca6613476ddd6
Ishika Bhaumik <54812782+ishikabhaumik@users.noreply.github.com>
2025-03-18 00:28:22 -0700

    Update react11.py

--- react_scripts/react11.py
+++ react_scripts/react11.py
@@ -1,34 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
 import requests
 
 def compute_react11(full_name):
-    #Keep the project small and simple
-
     repo_url = f'https://api.github.com/repos/{full_name}'
-
-  
     response = requests.get(repo_url)
 
-
     if response.status_code != 200:
-        return False
-
+        return 0  
 
     repo_data = response.json()
-
-
-    return repo_data.get("size", float('inf')) < 10000
-
-
-
-
-# In[ ]:
-
-
-
-
+    return 1 if repo_data.get("size", float('inf')) < 50000 else 0
